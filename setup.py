6ca10b2a8077414db0aedbac0fdd306431586a98
0ssw1 <seo@seowan.net>
2024-02-11 22:56:20 +0900

    Readme 수정

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', encoding='utf-8') as f:
 
 setup(name='gethwp', # 패키지 명
 
-version='1.1.0',
+version='1.1.1',
 
 long_description= long_description,
 long_description_content_type='text/markdown',
